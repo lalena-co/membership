408c53b6fab11b73b5e78e99222c679385c9ae66
keithpops <keith.poplawski@gmail.com>
2017-09-27 21:43:41 -0400

    updates index configuration

--- src/index.js
+++ src/index.js
@@ -1,8 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import promise from 'redux-promise';
+import registerServiceWorker from 'registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// state
+import { Provider } from 'react-redux';
+import { combineReducers, createStore, applyMiddleware, compose } from 'redux';
+import reducers from 'state/reducers';
+
+// routing
+import createHistory from 'history/createBrowserHistory';
+import { ConnectedRouter, routerReducer, routerMiddleware } from 'react-router-redux';
+
+import './index.css';
+import App from 'App';
+
+const history = createHistory();
+const router = routerMiddleware(history);
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+  combineReducers({
+    ...reducers,
+    router: routerReducer
+  }),
+  composeEnhancers(
+    applyMiddleware(promise, router),
+  ),
+);
+
+// render(
+ReactDOM.render(
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <App />
+    </ConnectedRouter>
+  </Provider>
+  , document.getElementById('root')
+);
+
 registerServiceWorker();
