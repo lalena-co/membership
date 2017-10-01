bd8bdc616cbdcbaf27b7dfda294387d43edac032
keithpops <keith.poplawski@gmail.com>
2017-09-30 22:08:33 -0400

    updates app configuration

--- src/App.js
+++ src/App.js
@@ -1,21 +1,118 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Switch, Redirect, Route } from 'react-router';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+import * as actions from 'state/actions';
+
+import Loading from 'components/Loading';
+import Header from 'components/Header';
+import Footer from 'components/Footer';
+
+// containers
+import { containers } from 'config';
+import EditContainer from 'containers/EditContainer';
+import LoginContainer from 'containers/LoginContainer';
+import MembershipContainer from 'containers/MembershipContainer';
+import SignUpContainer from 'containers/SignUpContainer';
+
+const {
+  Edit,
+  Login,
+  Membership,
+  SignUp,
+} = containers;
 
 class App extends Component {
-  render() {
+  componentWillMount = () => {
+    const {
+      authorizeApplication,
+      initializeDatabase,
+    } = this.props.actions;
+    authorizeApplication();
+    initializeDatabase();
+  }
+
+  currentUser = () => {
+    const { user } = this.props;
+    return user && user.uid;
+  }
+
+  renderRoutes = (location) => {
+    const currentUser = this.currentUser();
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <Switch
+        key={location.pathname}
+        location={location}
+      >
+        <Route
+          exact
+          path={currentUser ? Membership.path : Login.path}
+          component={currentUser ? MembershipContainer : LoginContainer}
+        />
+        {!currentUser ? (
+          <Route
+            exact
+            path={SignUp.path}
+            component={SignUpContainer}
+          />
+        ) : (
+          <Redirect
+            from={SignUp.path}
+            to={Membership.path}
+          />
+        )}
+        {currentUser ? (
+          <Route
+            exact
+            path={Edit.path}
+            component={EditContainer}
+          />
+        ) : (
+          <Redirect
+            from={Edit.path}
+            to={Login.path}
+          />
+        )}
+      </Switch>
+    );
+  }
+
+  renderLoading = () => {
+    return <Loading />;
+  }
+
+  render = () => {
+    const { actions, auth, router: { location } } = this.props;
+    const currentUser = this.currentUser();
+
+    return (
+      <div>
+        <Header />
+        {currentUser && (
+          <a onClick={actions.signOutUser}>{'Sign Out'}</a>
+        )}
+        { auth ? this.renderRoutes(location) : this.renderLoading()}
+        <Footer />
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return { ...state };
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    actions: bindActionCreators(
+      {
+        ...actions,
+      },
+      dispatch,
+    ),
+  };
+}
+
+export { App };
+export default connect(mapStateToProps, mapDispatchToProps)(App);
